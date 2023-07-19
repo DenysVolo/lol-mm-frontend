ed3de552e1f809847f4e48170441b0bf31cff878
Leatje <lea.fevrier@hotmail.com>
2023-07-19 21:49:21 +0100

    Leaderboard component setup

---
+++ app/src/components/LeaderboardComponent.vue
@@ -0,0 +1,49 @@
+<template>
+    <div>
+        <h1>Name of tournamement</h1>
+
+        <li v-for="team in teams" class="border border-gray-900 bg-gray-500 rounded-md p-5 flex justify-between items-center h-12">
+            <ul> {{ team.name }} </ul>
+            <ul> {{ team.points }} </ul>
+            <ul> {{ team.country }} </ul>
+        </li>
+
+        <button @click="fetchData">
+            Click me
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LeaderboardComponent",
+
+        data() {
+            return {
+                teams: [  ],
+            };
+        },
+
+        methods: {
+            fetchData() {
+                fetch('http://127.0.0.1:8081/get_leaderboard', {
+                    method: "GET"
+                })
+                .then((response) => {
+                    response.json().then((data) => {
+                        this.teams = data.data;
+                        console.log(data);
+                    });
+                })
+                .catch((err) => {
+                    console.error(err);
+                });
+            },
+        },
+
+        mounted() {
+            this.fetchData();
+        }
+    }
+
+</script>
