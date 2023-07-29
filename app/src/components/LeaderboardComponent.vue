6125cd57f075f46a51d491551916bc1ca46ca0a6
Max Clarke <maximilian-jc@live.com>
2023-07-29 11:48:25 +0100

    updated styling
    mobile responsive, colours, font

--- app/src/components/LeaderboardComponent.vue
+++ app/src/components/LeaderboardComponent.vue
@@ -9,14 +9,21 @@ const teamsStore = useTeamsStore();
 </script>
 
 <template>
-    <div>
-        <h1>Name of tournamement</h1>
-
-        <li v-for="team in teamsStore.teams" class="border border-gray-900 bg-gray-500 rounded-md p-5 flex justify-between items-center h-12">
-            {{ team.name }}, {{ team.points }}, {{ team.country }}
-        </li>
-
-        <button @click="teamsStore.testIncrement(); teamsStore.testAddTeam()">Add To Score and Add Team</button>
-        
+  <div class="w-5/6 mx-auto md:w-2/3 bg-cus-med-grey px-1 py-4 rounded-md font-bebas drop-shadow-xl border border-cus-yellow">
+    <div class="text-sm md:text-xl grid grid-cols-5 gap-2 text-center py-2 px-4 font-bold">
+      <div class="text-left">Position</div>
+      <div class="text-left">Team</div>
+      <div>Wins</div>
+      <div>Losses</div>
+      <div>Points</div>
     </div>
+    <div v-for="(team, index) in teamsStore.teams" class="text-sm grid grid-cols-5 gap-2 text-center md:text-xl py-2 px-4 rounded-sm" :class="index % 2 === 0 ? 'bg-cus-light-grey hover:bg-cus-light-grey-hover hover:drop-shadow-xl' : 'bg-cus-med-grey hover:bg-cus-med-grey-hover hover:drop-shadow-xl' ">
+      <div class="text-left">{{ index + 1 }} </div>
+      <div class="text-left">{{ team.name }}</div>
+      <div> 3 </div>
+      <div> 1 </div>
+      <div >{{ team.points }}</div>
+    </div>
+  </div>
+  <button @click="teamsStore.testIncrement(); teamsStore.testAddTeam()">Add To Score and Add Team</button>
 </template>
--- app/src/pages/LeaderboardPage.vue
+++ app/src/pages/LeaderboardPage.vue
@@ -1,12 +1,18 @@
 <template>
-    <div>
-        <h1>Leaderboard Page</h1>
-        <div>
-            <LeaderboardComponent/>
-        </div>
+  <div>
+    <div class="pt-6 md:pt-12 text-center text-6xl font-bold font-bebas">
+      <h1 class="inline-block px-2 pt-1 text-cus-gold">Leaderboard</h1>
     </div>
+    <div class="pt-6 md:pt-12">
+      <LeaderboardComponent/>
+    </div>
+  </div>
 </template>
 
 <script setup>
-    import LeaderboardComponent from '../components/LeaderboardComponent.vue'
+import LeaderboardComponent from '../components/LeaderboardComponent.vue'
 </script>
+
+<style>
+
+</style>
