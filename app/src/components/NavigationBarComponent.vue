55ea7198a9e08c1db6a733fb6651a2c6b5e4b96a
Lea Fevrier <105033423+leatje6299@users.noreply.github.com>
2023-08-06 14:08:46 +0200

    Moved nav bar to a component on its own
    I think there is probably a better way to do the page links (probably using the router?) but i am not sure

---
+++ app/src/components/NavigationBarComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+
+const links = [
+  { name: "Home", link: "/"},
+  { name: "Leaderboard", link: "/leaderboard"},
+  { name: "Teams", link: "/teams"}
+]
+</script>
+
+<template>
+  <div class="py-12 bg-cus-med-grey">
+    <nav>
+      <ul class="md:flex place-content-center mr-10">
+        <li class="md:mx-4" v-for="link in links">
+          <router-link class="text-3xl hover:text-cus-gold font-bold font-bebas tracking-wider" :to="link.link"> {{ link.name }} </router-link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
