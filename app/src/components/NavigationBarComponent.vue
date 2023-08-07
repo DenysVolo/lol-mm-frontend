7c6616b30c3419744b72441fb6c9c708e08a5ae0
Lea Fevrier <105033423+leatje6299@users.noreply.github.com>
2023-08-07 22:11:17 +0200

    Update to navbar routes

--- app/src/components/NavigationBarComponent.vue
+++ app/src/components/NavigationBarComponent.vue
@@ -1,19 +1,19 @@
 <script setup>
 import { RouterLink } from 'vue-router'
-
-const links = [
-  { name: "Home", link: "/"},
-  { name: "Leaderboard", link: "/leaderboard"},
-  { name: "Teams", link: "/teams"}
-]
 </script>
 
 <template>
-  <div class="py-12 bg-cus-med-grey">
+  <div class="py-12 bg-cus-dark-grey">
     <nav>
       <ul class="md:flex place-content-center mr-10">
-        <li class="md:mx-4" v-for="link in links">
-          <router-link class="text-3xl hover:text-cus-gold font-bold font-bebas tracking-wider" :to="link.link"> {{ link.name }} </router-link>
+        <li class="md:mx-4">
+          <router-link class="text-3xl hover:text-cus-gold font-bold font-bebas tracking-wider text-cus-white" :to="this.$router.getRoutes()[0].path"> {{ this.$router.getRoutes()[0].name }} </router-link>
+        </li>
+        <li class="md:mx-4">
+          <router-link class="text-3xl hover:text-cus-gold font-bold font-bebas tracking-wider text-cus-white" :to="this.$router.getRoutes()[1].path"> {{ this.$router.getRoutes()[1].name }} </router-link>
+        </li>
+        <li class="md:mx-4">
+          <router-link class="text-3xl hover:text-cus-gold font-bold font-bebas tracking-wider text-cus-white" :to="this.$router.getRoutes()[2].path"> {{ this.$router.getRoutes()[2].name }} </router-link>
         </li>
       </ul>
     </nav>
