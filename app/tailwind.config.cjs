b1d9fb7abe58093088ca33e26ac35a4c77052eae
Lea Fevrier <105033423+leatje6299@users.noreply.github.com>
2023-08-08 22:56:54 +0200

    Log In Component setup
    setup basics of log in component, no function actually working with buttons. need to also do the sign up switch instead of sign in and probably not leaving the component open on the home page but be so it opens on click

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -9,14 +9,13 @@ module.exports = {
             bebas: 'Bebas Neue'
         },
         colors: {
+            'cus-white' : '#F4F4F9',
             'cus-light-grey': '#434852',
             'cus-light-grey-hover': '#525864',
             'cus-med-grey' : '#353941',
             'cus-med-grey-hover' : '#646a78',
             'cus-dark-grey': '#1a1b1d',
             'cus-gold' : '#e39025',
-
-            'cus-white' : '#F4F4F9',
             'cus-light-blue' : '#B8DBD9'
         },
         extend: {},
